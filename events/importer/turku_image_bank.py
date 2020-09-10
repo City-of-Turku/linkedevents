4ccad595b539f2d63c4fa55ed90fe3f7d6463fe4
Anthon98 <anthon.kallankari@gmail.com>
2020-09-10 11:40:05 +0300

    Update turku_image_bank.py
    Debug

--- events/importer/turku_image_bank.py
+++ events/importer/turku_image_bank.py
@@ -7,7 +7,8 @@ from django.core.management import call_command
 from django.utils.module_loading import import_string
 from os import mkdir
 from os.path import abspath, join, dirname, exists, basename, splitext
-from events.models import Image, License, Organization
+from events.models import Image, License
+from django_orghierarchy.models import Organization
 from .sync import ModelSyncher
 from .base import Importer, register_importer
 
