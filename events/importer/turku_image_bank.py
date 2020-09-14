5fc30e5544361af026d38702bab5fa99bca6a188
Anthon98 <anthon.kallankari@gmail.com>
2020-09-14 12:09:46 +0300

    Update turku_image_bank.py
    Nullifying publisher for an experiment.

--- events/importer/turku_image_bank.py
+++ events/importer/turku_image_bank.py
@@ -1,4 +1,4 @@
-#Dependencies
+# Dependencies
 import logging
 import os
 from django import db
@@ -21,7 +21,7 @@ __iter__ = iter
 __next__ = next
 
 
-logger = logging.getLogger(__name__) 
+logger = logging.getLogger(__name__)
 
 curFileExt = basename(__file__)
 curFile = splitext(curFileExt)[0]
@@ -50,6 +50,7 @@ def get_create_image(ob, args):
     except:
         logger.warn("Image update_or_create did NOT pass: "+ob+" correctly. Argument/Arguments incompatible.")
 
+
 def get_create_ds(ob, args):
     try:
         ds, _ = DataSource.objects.update_or_create(defaults=args[1], **args[0])
@@ -65,30 +66,32 @@ def get_create_organization(ob, args):
     except:
         logger.warn("Organization update_or_create did NOT pass: "+ob+" correctly. Argument/Arguments incompatible.")
 
+
 def get_create_organizationclass(ob, args):
     try:
         orgclass, _ = OrganizationClass.objects.update_or_create(defaults=args[1], **args[0])
         return orgclass
     except:
-        logger.warn("OrganizationClass update_or_create did NOT pass: "+ob+" correctly. Argument/Arguments incompatible.")
+        logger.warn("OrganizationClass update_or_create did NOT pass: " +
+                    ob+" correctly. Argument/Arguments incompatible.")
 
 
 def process(self):
-    #DataSource
-    # -> datasources contains all top level datasource objects; no data_source defined. 
+    # DataSource
+    # -> datasources contains all top level datasource objects; no data_source defined.
     try:
         self.cc_by_license = License.objects.get(id='cc_by')
     except License.DoesNotExist:
         self.cc_by_license = None
 
     datasources = {
-        'img_ds':[dict(id="image", user_editable=True), dict(name='Kuvapankki')],
-        'org':[dict(id="org", user_editable=True), dict(name='Ulkoa tuodut organisaatiotiedot')]
+        'img_ds': [dict(id="image", user_editable=True), dict(name='Kuvapankki')],
+        'org': [dict(id="org", user_editable=True), dict(name='Ulkoa tuodut organisaatiotiedot')]
     }
     return_ds = [get_create_ds(keys, values) for keys, values in datasources.items()]
 
     ds_orgs_class = {
-        'kvpankkiclass':[dict(origin_id='15', data_source=return_ds[1]), dict(name='Kuvapankki')],
+        'kvpankkiclass': [dict(origin_id='15', data_source=return_ds[1]), dict(name='Kuvapankki')],
     }
     return_orgclass_ds = [get_create_organizationclass(keys, values) for keys, values in ds_orgs_class.items()]
 
@@ -96,19 +99,19 @@ def process(self):
     rgc = return_orgclass_ds.__iter__()
 
     org_arr = {
-        'image_org':[dict(origin_id='1500', data_source=return_ds[0], classification_id="org:15"), dict(name='Kuvapankki')],
+        'image_org': [dict(origin_id='1500', data_source=return_ds[0], classification_id="org:15"), dict(name='Kuvapankki')],
     }
     return_org = [get_create_organization(keys, values) for keys, values in org_arr.items()]
     ro = return_org.__iter__()
 
     imgs = {
-        'img':[dict(license=self.cc_by_license, data_source=return_ds[0], publisher=return_org[0]), dict(url='https://kalenteri.turku.fi/sites/default/files/styles/event_node/public/images/event_ext/sadonkorjuutori.jpg', name='',photographer_name='',alt_text='')]
+        'img': [dict(license=self.cc_by_license, data_source=return_ds[0], publisher=None), dict(url='https://kalenteri.turku.fi/sites/default/files/styles/event_node/public/images/event_ext/sadonkorjuutori.jpg', name='', photographer_name='', alt_text='')]
     }
     return_img = [get_create_image(keys, values) for keys, values in imgs.items()]
     rdi = return_img.__iter__()
 
     try:
-        return { # -> Class attribute names go here. Could return an already sorted dictionary if need be.
+        return {  # -> Class attribute names go here. Could return an already sorted dictionary if need be.
             'data_source': rds.__next__(),
             'data_source_org': rds.__next__(),
             'org_class': rgc.__next__(),
@@ -123,6 +126,7 @@ def process(self):
 class ImageBankImporter(Importer):
     name = curFile
     supported_languages = ['fi', 'sv']
+
     def setup(self):
         for k, v in process(self).items():
             print("yes")
