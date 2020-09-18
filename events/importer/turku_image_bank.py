17ea118d0bc47d0f1f21c7a2802fbc4c7c617583
Anthon98 <anthon.kallankari@gmail.com>
2020-09-18 10:53:05 +0300

    Renamed new image bank importer and removed the old version.

--- events/importer/turku_image_bank.py
+++ events/importer/turku_image_bank.py
@@ -1,4 +1,3 @@
-# Dependencies
 import logging
 import os
 from django import db
@@ -15,14 +14,7 @@ from .base import Importer, register_importer
 if not exists(join(dirname(__file__), 'logs')):
     mkdir(join(dirname(__file__), 'logs'))
 
-
-__setattr__ = setattr
-__iter__ = iter
-__next__ = next
-
-
 logger = logging.getLogger(__name__)
-
 curFileExt = basename(__file__)
 curFile = splitext(curFileExt)[0]
 
@@ -36,108 +28,60 @@ logFile.setFormatter(
     )
 )
 logFile.setLevel(logging.DEBUG)
-
-
 logger.addHandler(
     logFile
 )
 
 
-def get_create_image(ob, args):
-    print(ob, args)
-    try:
-        imge, _ = Image.objects.update_or_create(defaults=args[1], **args[0])
-        return imge
-    except:
-        logger.warn("Image update_or_create did NOT pass: "+ob+" correctly. Argument/Arguments incompatible.")
-
-
-def get_create_ds(ob, args):
-    try:
-        ds, _ = DataSource.objects.update_or_create(defaults=args[1], **args[0])
-        return ds
-    except:
-        logger.warn("DataSource update_or_create did NOT pass: "+ob+" correctly. Argument/Arguments incompatible.")
-
-
-def get_create_organization(ob, args):
-    try:
-        org, _ = Organization.objects.update_or_create(defaults=args[1], **args[0])
-        return org
-    except:
-        logger.warn("Organization update_or_create did NOT pass: "+ob+" correctly. Argument/Arguments incompatible.")
-
-
-def get_create_organizationclass(ob, args):
-    try:
-        orgclass, _ = OrganizationClass.objects.update_or_create(defaults=args[1], **args[0])
-        return orgclass
-    except:
-        logger.warn("OrganizationClass update_or_create did NOT pass: " +
-                    ob+" correctly. Argument/Arguments incompatible.")
-
-
-def process(self):
-    # DataSource
-    # -> datasources contains all top level datasource objects; no data_source defined.
-    try:
-        self.cc_by_license = License.objects.get(id='cc_by')
-    except License.DoesNotExist:
-        self.cc_by_license = None
-
-    datasources = {
-        'img_ds': [dict(id="image", user_editable=True), dict(name='Kuvapankki')],
-        'org': [dict(id="org", user_editable=True), dict(name='Ulkoa tuodut organisaatiotiedot')]
-    }
-    return_ds = [get_create_ds(keys, values) for keys, values in datasources.items()]
-    print(return_ds)
-
-    ds_orgs_class = {
-        'kvpankkiclass': [dict(origin_id='15', data_source=return_ds[1]), dict(name='Kuvapankki')],
-    }
-    return_orgclass_ds = [get_create_organizationclass(keys, values) for keys, values in ds_orgs_class.items()]
-
-    rds = return_ds.__iter__()
-    rgc = return_orgclass_ds.__iter__()
-
-    org_arr = {
-        'image_org': [dict(origin_id='1500', data_source=return_ds[0], classification_id="org:15"), dict(name='Kuvapankki')],
-    }
-    return_org = [get_create_organization(keys, values) for keys, values in org_arr.items()]
-    ro = return_org.__iter__()
-
-    imgs = {
-        'img': [dict(license=self.cc_by_license, data_source=return_ds[0], publisher=return_org[0], url='https://kalenteri.turku.fi/sites/default/files/styles/event_node/public/images/event_ext/sadonkorjuutori.jpg'), dict(name='', photographer_name='', alt_text='')],
-        'img_2': [dict(license=self.cc_by_license, data_source=return_ds[0], publisher=return_org[0], url='https://kalenteri.turku.fi/sites/default/files/styles/event_node/public/images/event_ext/img_2738.jpg'), dict(name='', photographer_name='', alt_text='')],
-        'img_3': [dict(license=self.cc_by_license, data_source=return_ds[0], publisher=return_org[0], url='https://kalenteri.turku.fi/sites/default/files/styles/event_node/public/images/event_ext/66611781_2613563701989600_82393011928956928_n_7.jpg'), dict(name='', photographer_name='', alt_text='')],
-        'img_4': [dict(license=self.cc_by_license, data_source=return_ds[0], publisher=return_org[0], url='https://kalenteri.turku.fi/sites/default/files/styles/event_node/public/images/event_ext/nuorten_viikonloppu_turun_seudun_tapahtumakalenterin_kuva_yhdistetty.jpg'), dict(name='', photographer_name='', alt_text='')]
-    }
-    return_img = [get_create_image(keys, values) for keys, values in imgs.items()]
-    rdi = return_img.__iter__()
-    print(return_img)
-
-    try:
-        return {  # -> Class attribute names go here. Could return an already sorted dictionary if need be.
-            'data_source': rds.__next__(),
-            'data_source_org': rds.__next__(),
-            'org_class': rgc.__next__(),
-            'organization': ro.__next__(),
-            'a1': rdi.__next__(),
-            'a2': rdi.__next__(),
-            'a3': rdi.__next__(),
-            'a4': rdi.__next__(),
-        }
-    except:
-        logger.warn("Stop iteration error when returning process function items.")
-
-
 @register_importer
 class ImageBankImporter(Importer):
+    # Class dependencies.
     name = curFile
     supported_languages = ['fi', 'sv']
 
     def setup(self):
-        for k, v in process(self).items():
-            print("yes")
-            __setattr__(self, k, v)
-            logger.info("ImageBankImporter image created: "+k)
+
+        # Datasources.
+        self.data_source, _ = DataSource.objects.update_or_create(
+            defaults=dict(name='Kuvapankki'), **dict(id='image', user_editable=True))
+        self.data_source_org, _ = DataSource.objects.update_or_create(
+            defaults=dict(name='Ulkoa tuodut organisaatiotiedot'), **dict(id='org', user_editable=True))
+
+        # Organization classes.
+        self.organizationclass_kvpankki, _ = OrganizationClass.objects.update_or_create(
+            defaults=dict(name='Kuvapankki'), **dict(origin_id='15', data_source=self.data_source_org))
+
+        # Organizations.
+        self.organization, _ = Organization.objects.update_or_create(
+            defaults=dict(name='Kuvapankki'), **dict(origin_id='1500', data_source=self.data_source, classification_id='org:15'))
+
+        try:
+            self.cc_by_license = License.objects.get(id='cc_by')
+        except License.DoesNotExist:
+            self.cc_by_license = None
+
+        # Default Image URL's.
+        self.image_1, _ = Image.objects.update_or_create(
+            defaults=dict(name='', photographer_name='', alt_text=''), **dict(
+                license=self.cc_by_license,
+                data_source=self.data_source,
+                publisher=self.organization,
+                url='https://kalenteri.turku.fi/sites/default/files/styles/event_node/public/images/event_ext/sadonkorjuutori.jpg'))
+        self.image_2, _ = Image.objects.update_or_create(
+            defaults=dict(name='', photographer_name='', alt_text=''), **dict(
+                license=self.cc_by_license,
+                data_source=self.data_source,
+                publisher=self.organization,
+                url='https://kalenteri.turku.fi/sites/default/files/styles/event_node/public/images/event_ext/img_2738.jpg'))
+        self.image_3, _ = Image.objects.update_or_create(
+            defaults=dict(name='', photographer_name='', alt_text=''), **dict(
+                license=self.cc_by_license,
+                data_source=self.data_source,
+                publisher=self.organization,
+                url='https://kalenteri.turku.fi/sites/default/files/styles/event_node/public/images/event_ext/66611781_2613563701989600_82393011928956928_n_7.jpg'))
+        self.image_4, _ = Image.objects.update_or_create(
+            defaults=dict(name='', photographer_name='', alt_text=''), **dict(
+                license=self.cc_by_license,
+                data_source=self.data_source,
+                publisher=self.organization,
+                url='https://kalenteri.turku.fi/sites/default/files/styles/event_node/public/images/event_ext/nuorten_viikonloppu_turun_seudun_tapahtumakalenterin_kuva_yhdistetty.jpg'))
