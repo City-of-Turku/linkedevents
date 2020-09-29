0b4e7f00584bdc24e3cdffac2ff640ec8fe7b7c3
Anthon98 <anthon.kallankari@gmail.com>
2020-09-29 13:48:11 +0300

    Update turku_old_events.py

--- events/importer/turku_old_events.py
+++ events/importer/turku_old_events.py
@@ -513,7 +513,8 @@ class TurkuOriginalImporter(Importer):
             
             if eventTku.get('event_categories', None):
                 node_type = eventTku['event_categories']
-                logger.info(node_type[0])
+                logger.info(type(node_type))
+
                 if node_type == 'Virtual events':
                     logger.info("This is a Virtual Event.")
                     time.sleep(2)
